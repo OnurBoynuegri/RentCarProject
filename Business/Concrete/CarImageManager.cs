53646486424539bdb0099f91618e9f3f5a8ba25d
OnurBoynuegri <onurboynuegri@hotmail.com>
2021-07-02 21:30:24 +0300

    CarImage entitysi eklendi ve servisleri yazıldı.

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
+        public IResult Add(CarImage carImage)
+        {
+            _carImageDal.Add(carImage);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            _carImageDal.Delete(carImage);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            _carImageDal.GetAll();
+            return new SuccessDataResult<List<CarImage>>();
+        }
+
+        public IDataResult<CarImage> GetById(int carImageId)
+        {
+            _carImageDal.Get(i => i.ImageId == carImageId);
+            return new SuccessDataResult<CarImage>();
+        }
+
+        public IResult Update(CarImage carImage)
+        {
+            _carImageDal.Update(carImage);
+            return new SuccessResult();
+        }
+    }
+}
--- DataAccess/Concrete/EntityFramework/RentCarDBContext.cs
+++ DataAccess/Concrete/EntityFramework/RentCarDBContext.cs
@@ -19,6 +19,7 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Rental> Rentals { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<CarImage> CarImages { get; set; }
 
     }
 }
